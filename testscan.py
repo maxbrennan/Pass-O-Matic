1d42b93b8ce4d677f107e0076ae4bc15b1a6b401
mrbdahlem <bdahlem@blueChrome>
2022-08-13 12:42:18 -0700

    working scan sample

--- testscan.py
+++ testscan.py
@@ -1,11 +1,12 @@
 import board
 import busio
 
-RX = board.GP9
-TX = board.GP8
+RX = board.GP5
+TX = board.GP4
 
 scanner_uart = busio.UART(TX, RX, baudrate=9600)
 
 while True:
     data = scanner_uart.read(32)  # read up to 32 bytes
-    print(data)  # this is a bytearray type
+    if data is not None:
+        print(data)  # this is a bytearray type
