4da489614a169887930af5e5e925e5840dec2dc9
mrbdahlem <bdahlem@blueChrome>
2022-08-31 14:41:52 -0700

    record activity returns true if recorded

--- activity.py
+++ activity.py
@@ -1,4 +1,4 @@
-def record_student(student, time):
+def record_student(student, time) -> bool:
     time_str = "{}-{:02}-{:02}T{:02}:{:02}".format(time.tm_year, time.tm_mon, time.tm_mday,
                                        time.tm_hour, time.tm_min)
     id = str(student["id"])
@@ -9,5 +9,6 @@ def record_student(student, time):
     try:
         with open("activity.csv", mode="a", encoding="utf-8") as actfile:
             actfile.write(record + "\n")
+        return True
     except:
-        pass
+        return False
